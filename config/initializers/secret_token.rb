83d662fcfb5e21d4a63402af87ef50d19644ce8d
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-03-23 09:29:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IPstore::Application.config.secret_token = 'f8294ddd82320c9fe8946c6ec46eec978f6510bf556af74e5d26f2d2280f7fb70177bdb58fa3d75535b06f6138d494479144957884dbbe23f900c6dd5ca6c39d'
