22272f1b008aa23db7658aa383efcdb060642337
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-03-23 15:01:04 -0400

    Adding and requiring JSON gem

--- app/controllers/ip_addresses_controller.rb
+++ app/controllers/ip_addresses_controller.rb
@@ -1,20 +1,21 @@
 class IpAddressesController < ApplicationController
 require "net/http"
+require "json"
 
   def index
 
   end
 
   def show
-
+  
   end
 
-  def new
- 
-  end
+  def create
+   
+    end
 
   def destroy
-   
+ 
   end
 end
 
