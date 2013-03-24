eef37ee8fc1f7c1b5562604cd5b3676a1ac233a5
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-03-24 09:51:38 -0400

    Adding 'new' action, mocking up solutions

--- app/controllers/ip_addresses_controller.rb
+++ app/controllers/ip_addresses_controller.rb
@@ -3,19 +3,33 @@ require "net/http"
 require "json"
 
   def index
-
+  	# ips = IpAddress.all 
   end
 
   def show
-  
+  	# ip = IPAddress.find(params[:ip])
+  end
+
+  def new
+    # @ip = IpAddress.new
   end
 
   def create
-   
-    end
+    # ip = IpAddress.new(params[:ip_address])
+    # JSON.parse(ip)
+    # ips = IpAddress.all
+    
+    # #need to return 200
+    #   unless ip = IpAddress.find(params[:ip])
+    #     ips.push(ip)
+    #   end
+    # ip.code
+  end
 
   def destroy
- 
+    # ip = IpAddress.all
+    # ip.destroy
+    # head :ok
   end
 end
 
