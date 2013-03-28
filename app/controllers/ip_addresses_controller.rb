aff79eb46f9d568965764d1d2d03eca8481d3335
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-03-28 11:05:27 -0400

    Passing tests

--- app/controllers/ip_addresses_controller.rb
+++ app/controllers/ip_addresses_controller.rb
@@ -8,16 +8,16 @@ require "json"
   end
 
   def show
-  	IpAddress.find(params[:ip])
+  	@ip_address = IpAddress.find_by_ip(params[:ip])
+    render json: @ip_address.ip
   end
 
   def create
-    existing_addresses = IpAddress.pluck(:ip)
-
     params[:_json].each do |ip|
-      IpAddress.create(ip: ip) unless existing_addresses.include?(ip)
+      IpAddress.create(ip: ip) unless IpAddress.pluck(:ip).include?(ip)
     end
-    render json: existing_addresses
+
+    render json: IpAddress.pluck(:ip)
   end
 
   def destroy
--- test/tests.rb
+++ test/tests.rb
@@ -23,14 +23,14 @@ class TestIpAddressServer < Test::Unit::TestCase
 
   def test_get_ip
     r = @http.get("/ip/get/1.2.3.4")
-    assert_equal("500", r.code)  #500 is server error...need to customize response in def show
+    assert_equal("500", r.code)  
 
     r = @http.post("/ip/add", '["1.2.3.4","2.3.4.5"]', {"content-type" => "application/json"})
     assert_equal(r.code, "200")
 
     r = @http.get("/ip/get/1.2.3.4")
-    assert_equal(r.code, "200")
-    assert_equal(r.body, "1.2.3.4")
+    assert_equal("200", r.code)
+    assert_equal("1.2.3.4", r.body)
   end
 
   def test_add_ip
